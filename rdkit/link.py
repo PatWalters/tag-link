4d7c74a2189eeed7a49aa20bdf8973484fcf6b80
PatWalters <wpwalters@gmail.com>
2018-08-28 08:56:59 -0400

    Added RDKit versions

---
+++ rdkit/link.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+"""Usage: link.py --file1 FILE1 --link1 LINK1 --file2 FILE2 --link2 LINK2 --out OUTFILE
+
+--file1 FILE1 first file to be linked
+--link1 LINK1 atom type in file1
+--file2 FILE2 second file to be linked
+--link2 LINK2 atom type in file2
+--out OUTFILE output file
+"""
+
+from rdkit import Chem
+from docopt import docopt
+
+
+def map_attached_atom(mol, atomic_num, map_idx):
+    """
+    Assign an AtomMapNum to the atom attached to an atom adjacent to an atom with a specified atomic number
+    Also deletes the atom with the specified atomic number
+    E.g. with [Au]CCC as input generated [C:1]CC
+    :param mol: input molecule
+    :param atomic_num: atomic number of the atom to be removed
+    :param map_idx: map index to assign to the the adjacent atom
+    :return: modified molecule
+    """
+    rw_mol = Chem.RWMol(mol)
+    to_remove = -1
+    for atm in rw_mol.GetAtoms():
+        if atm.GetAtomicNum() == atomic_num:
+            to_remove = atm.GetIdx()
+            nbr = [x.GetOtherAtom(atm) for x in atm.GetBonds()][0]
+            nbr.SetAtomMapNum(map_idx)
+    if to_remove >= 0:
+        rw_mol.RemoveAtom(to_remove)
+    return rw_mol
+
+
+def link_molecules(in_mol_1, atm_type_1, in_mol_2, atm_type_2):
+    """
+    Link two molecules with a single bond
+    E.g. [Au]CCC + [Au]c1ccccc1 -> CCC-c1ccccc1
+    :param in_mol_1: the first molecule
+    :param atm_type_1: the atom type of the link atom in the first molecule
+    :param in_mol_2: the second molecule
+    :param atm_type_2: the atom type of the link atom in the second molecule
+    :return: modified molecule
+    """
+    mol_1 = map_attached_atom(in_mol_1, atm_type_1, 1)
+    mol_2 = map_attached_atom(in_mol_2, atm_type_2, 2)
+    new_mol = Chem.RWMol(Chem.CombineMols(mol_1, mol_2))
+    m1 = -1
+    m2 = -1
+    for atm in new_mol.GetAtoms():
+        if atm.GetAtomMapNum() == 1:
+            m1 = atm.GetIdx()
+            atm.SetAtomMapNum(0)
+        if atm.GetAtomMapNum() == 2:
+            m2 = atm.GetIdx()
+            atm.SetAtomMapNum(0)
+    print(m1,m2)
+    if m1 >= 0 and m2 >= 0:
+        new_mol.AddBond(m1, m2, order=Chem.rdchem.BondType.SINGLE)
+    return new_mol
+
+
+def get_atomic_num(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    atm = [x for x in mol.GetAtoms()][0]
+    return atm.GetAtomicNum()
+
+
+if __name__ == "__main__":
+    cmd_input = docopt(__doc__)
+    file_name1 = cmd_input.get("--file1")
+    file_name2 = cmd_input.get("--file2")
+    link1 = cmd_input.get("--link1")
+    link2 = cmd_input.get("--link2")
+    ofs = open(cmd_input.get("--out"), "w")
+
+    suppl_1 = Chem.SmilesMolSupplier(file_name1)
+    suppl_2 = Chem.SmilesMolSupplier(file_name2)
+
+    mol_list1 = [x for x in suppl_1]
+    mol_list2 = [x for x in suppl_2]
+
+    type_1 = get_atomic_num(link1)
+    type_2 = get_atomic_num(link2)
+
+    for m1 in mol_list1:
+        for m2 in tqdm(mol_list2):
+            print(link_molecules(m1, link1, m2, link2))
--- rdkit/tag.py
+++ rdkit/tag.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-"""Usage: tag.py --in INFILE_NAME --out OUTFILE_NAME --smarts SMARTS --tag TAG
+"""Usage:
+tag.py --in INFILE_NAME --out OUTFILE_NAME --smarts SMARTS --tag TAG [--replace]
 
 --in INFILE_NAME input SMILES file
 --out OUTFILE_NAME output SMILES file
@@ -14,15 +15,44 @@ from docopt import docopt
 from tqdm import tqdm
 
 
-def tag_molecule(mol, query, replacement_type):
+def tag_molecule(mol, query, atomic_num, replace):
+    if replace:
+        return replace_fragment(mol, query, atomic_num)
+    else:
+        return attach_atom(mol, query, atomic_num)
+
+
+def attach_atom(mol, query, atomic_num):
     result_smiles_list = []
     match_list = mol.GetSubstructMatches(query)
     for idx in [x[0] for x in match_list]:
         new_mol = Chem.RWMol(mol)
-        new_idx = new_mol.AddAtom(Chem.Atom(replacement_type))
+        new_idx = new_mol.AddAtom(Chem.Atom(atomic_num))
         new_mol.AddBond(idx, new_idx, order=Chem.rdchem.BondType.SINGLE)
         result_smiles_list.append(Chem.MolToSmiles(new_mol, True))
     return result_smiles_list
+
+
+def get_neighbor_atoms(atm):
+    return [x.GetOtherAtom(atm).GetIdx() for x in atm.GetBonds()]
+
+
+def replace_fragment(mol,query,atomic_num):
+    new_mol = Chem.RWMol(mol)
+    match_list = new_mol.GetSubstructMatches(query)
+    if len(match_list) == 0:
+        return None
+    for idx in match_list[0]:
+        atm = new_mol.GetAtomWithIdx(idx)
+        nbr_list = get_neighbor_atoms(atm)
+        good_neighbors = [x for x in nbr_list if x not in match_list[0]]
+        if len(good_neighbors):
+            nbr_atm = good_neighbors[0]
+            new_idx = new_mol.AddAtom(Chem.Atom(atomic_num))
+            new_mol.AddBond(nbr_atm, new_idx, order=Chem.rdchem.BondType.SINGLE)
+            new_mol = Chem.DeleteSubstructs(new_mol,query)
+            return [Chem.MolToSmiles(new_mol)]
+    return None
 
 
 def main(arg_string):
@@ -31,6 +61,7 @@ def main(arg_string):
     output_file = cmd_input.get("--out")
     smarts = cmd_input.get("--smarts")
     tag = cmd_input.get("--tag")
+    do_replace = cmd_input.get("--replace")
 
     tag_mol = Chem.MolFromSmiles(tag)
     if tag_mol is None:
@@ -53,7 +84,7 @@ def main(arg_string):
     ofs = open(output_file, "w")
     tagged_smiles_set = set()
     for in_mol in tqdm(suppl):
-        tagged_smiles_list = tag_molecule(in_mol, query_mol, 79)
+        tagged_smiles_list = tag_molecule(in_mol, query_mol, 79, do_replace)
         for smiles in tagged_smiles_list:
             if smiles not in tagged_smiles_set:
                 tagged_smiles_set.add(smiles)
